9169d2e37556dc981eb9c8d3e1691cc352d33e95
고윤혁 <sksgur3217@gmail.com>
2022-12-27 02:39:02 +0900

    장바구니, 위시리스트 이제 시작합니다.

--- repositories/carts.repository.js
+++ repositories/carts.repository.js
@@ -1,7 +1,19 @@
 class CartRepository {
-  constructor() {
-    // todo
+  constructor(ItemModel, CartModel) {
+    this.cartModel = CartModel;
+    this.itemModel = ItemModel;
   }
+
+  addItemInCart = async (itemId) => {
+    const userId = 1;
+
+    const addItemInCart = this.cartModel.create({
+      itemId: itemId,
+      userId: userId,
+    });
+
+    return addItemInCart;
+  };
 }
 
 module.exports = CartRepository;
