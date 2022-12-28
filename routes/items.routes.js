012d1429cb88b0bd5ddb9f03818938d168c64539
고윤혁 <sksgur3217@gmail.com>
2022-12-28 12:41:52 +0900

    merge 후 일부 코드 수정

--- routes/items.routes.js
+++ routes/items.routes.js
@@ -8,7 +8,7 @@ router
   .post('/', upload.array('mainImage'), itemController.createItem) // 상품 추가, 끝
   .get('/:gender/:theme', itemController.findAllItem) // 테마/카테고리 상품 조회, 예성
   .get('/:itemId', itemController.getItemDetailInformation) // 상품 상세 조회, 끝
-  .patch('/:itemId', upload.array('mainImage'), itemController.patchItem); // 상품 수정
-  .delete('/:itemId', itemController); // 상품 삭제, 진솔
+  .patch('/:itemId', upload.array('mainImage'), itemController.patchItem) // 상품 수정
+  .delete('/:itemId', itemController.deleteItem); // 상품 삭제, 진솔
 
 module.exports = router;
